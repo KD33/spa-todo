f80400e2dd62d3af1df74d0ab7a43f6551ffb99f
KD33 <Walshk94@gmail.com>
2018-05-31 17:57:56 +0000

    Strikethrough for completed tasks

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,7 +3,9 @@
 
     function taskHtml(task){
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox" '+
+      var liClass = task.done ? "completed": "";
+      var liElement = '<li id="listItem-' + task.id + '" class="' + liClass + '">' +
+       '<div class="view"><input class="toggle" type="checkbox"' +
       "data-id='" + task.id + "'" +
       checkedStatus +
       '/><label>' +
@@ -22,6 +24,11 @@
         task: {
           done: doneValue
         }
+      }).success(function(data){
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
       });
 
     }
